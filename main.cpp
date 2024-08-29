36b2e93d9036b3240b0b1c3e9b7357dda4316792
bckapokharel <166565787+bckapokharel@users.noreply.github.com>
2024-08-29 16:05:32 -0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+// main.cpp
+#include "main.h" // Include the header file
 
 int main() {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+    std::cout << "Hello, World!" << std::endl; // Print "Hello, World!" to the console
+    return 0; // Indicate that the program ended successfully
 }
